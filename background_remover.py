0ec70ca763a18393685e0cc75b36db5b2beb150f
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2023-01-23 12:21:50 +0200

    Add typing

--- background_remover.py
+++ background_remover.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import io
+from typing import Tuple, List
+
 import requests
 
 import torch
@@ -13,30 +15,31 @@ class BgRemover:
 
     def __init__(
             self,
-            model,
-            preprocessor,
-            device,
+            model: torch.nn.Module,
+            preprocessor: transforms.transforms.Compose,
+            device: str,
     ):
         self.model = model
         self.preprocessor = preprocessor
         self.device = device
 
-    def mask_background(self, img) -> np.ndarray:
+    def mask_background(self, img: Image.Image) -> np.ndarray:
         input_tensor = self.preprocessor(img).unsqueeze(0).to(self.device)
         with torch.no_grad():
             output = self.model(input_tensor)
         mask = output["out"][0].argmax(0).detach().cpu().numpy().astype(np.uint8)
         bin_mask = np.expand_dims(mask, 2)
         bin_mask = np.concatenate([bin_mask, bin_mask, bin_mask], axis=-1)
+        # set background to all zeroes which is black color
         masked = np.where(bin_mask == 0, 0, np.array(img))
         return masked
 
-    def remove_background(self, img, save_path) -> None:
+    def remove_background(self, img: Image.Image, save_path: Path) -> None:
         masked = self.mask_background(img)
         Image.fromarray(masked).save(save_path)
 
 
-def read_images(path_pattern: str):
+def read_images(path_pattern: str) -> Tuple[List[Path], List[Image.Image]]:
     path = Path()
     paths = path.glob(path_pattern)
     images = []
@@ -51,7 +54,7 @@ def read_images(path_pattern: str):
     return keys, images
 
 
-def img_from_url(url):
+def img_from_url(url: str) -> Image.Image:
     response = requests.get(url)
     img = Image.open(io.BytesIO(response.content))
     return img
@@ -81,9 +84,3 @@ def process_dir(in_dir: str, out_dir: str, remover: BgRemover) -> None:
         new_path = out_dir / k.parts[1] / k.parts[2]
         new_path.parent.mkdir(exist_ok=True)
         remover.remove_background(img, new_path)
-
-
-
-
-
-
